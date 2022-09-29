0c2b1f589b5d2a9bf4288813fe6d69f6595c9062
mahmudunnaby <mahmudunnabyemon0@gmail.com>
2022-09-29 18:33:07 +0600

    catagory responsive

--- src/components/Catagories/Catagories.js
+++ src/components/Catagories/Catagories.js
@@ -6,26 +6,28 @@ const Catagories = () => {
             <h1 className=' mt-20 text-4xl font-bold mb-7'>Shop by Categories</h1>
 
             <div>
-                <div className="grid grid-rows-3 grid-flow-col gap-4">
-                    <div className="row-span-3 h-[686px] ">
-                        <div className=" h-full bg-[url('/public/1.jpg')] lg:bg-cover p-7">
+                <div className="grid lg:grid-rows-2 grid-rows-3 grid-flow-col gap-4 ">
+                    {/* A ROW */}
+                    <div className="row-span-3 lg:h-[686px] h-[300px] ">
+                        <div className=" h-full bg-[url('/public/1.jpg')] lg:bg-cover bg-contain p-7">
                             <h1 className=' text-white text-2xl font-bold'>Womens Pants</h1>
                             <h1 className=' text-white text-lg font-bold'>Explore</h1>
                         </div>
                     </div>
-                    <div className="row-span-3  ">
+                    {/* A ROW */}
+                    <div className="lg:row-span-3  col-span-2">
                         <div className="col-span-2 ">
-                            <div className="row-span-3 h-[330px]">
-                                <div className=" h-full bg-[url('/public/17.jpg')] lg:bg-cover p-7">
+                            <div className="row-span-3 lg:h-[330px] h-[150px]">
+                                <div className=" h-full bg-[url('/public/17.jpg')] md:bg-cover bg-contain  p-7">
                                     <h1 className=' text-white text-2xl font-bold'>Mens Jacket</h1>
                                     <h1 className=' text-white text-lg font-bold'>Explore</h1>
                                 </div>
                             </div>
 
                         </div>
-                        <div className="col-span-2  ">
-                            <div className="row-span-3 h-[330px]">
-                                <div className=" h-full bg-[url('/public/9.jpg')] lg:bg-cover bg-center p-7">
+                        <div className="lg:col-span-2  ">
+                            <div className="row-span-3 lg:h-[330px] h-[150px]">
+                                <div className=" h-full bg-[url('/public/9.jpg')] md:bg-cover bg-contain md:bg-center p-7">
                                     <h1 className=' text-white text-2xl font-bold'>Mens Jacket</h1>
                                     <h1 className=' text-white text-lg font-bold'>Explore</h1>
                                 </div>
@@ -34,7 +36,7 @@ const Catagories = () => {
                     </div>
                     <div className="col-span-2 ">
                         <div className="col-span-2  ">
-                            <div className="row-span-3 h-[330px]">
+                            <div className="row-span-3 lg:h-[330px] h-[300px]">
                                 <div className=" h-full bg-[url('/public/10.jpg')] lg:bg-cover bg-center p-7">
                                     <h1 className=' text-white text-2xl font-bold'>Mens Jacket</h1>
                                     <h1 className=' text-white text-lg font-bold'>Explore</h1>
@@ -43,23 +45,25 @@ const Catagories = () => {
 
                         </div>
                     </div>
+                    {/* A ROW
+                     */}
                     <div className="row-span-1 col-span-2 ">
-                        <div className="grid grid-cols-2 gap-4">
+                        <div className="grid lg:grid-cols-2 grid-cols-1  gap-4">
                             <div> <div className="col-span-2  ">
-                                <div className="row-span-3 h-[330px]">
-                                    <div className=" h-full bg-[url('/public/3.jpg')] lg:bg-cover bg-center p-7">
-                                        <h1 className=' text-white text-2xl font-bold'>Mens Jacket</h1>
-                                        <h1 className=' text-white text-lg font-bold'>Explore</h1>
+                                <div className="row-span-3 lg:h-[330px] h-[150px]">
+                                    <div className=" h-full bg-[url('/public/3.jpg')] lg:bg-cover bg-center  p-7">
+                                        <h1 className=' text-white lg:text-2xl text-sm font-bold'>Mens Jacket</h1>
+                                        <h1 className=' text-white lg:text-lg text-xs font-bold'>Explore</h1>
                                     </div>
                                 </div>
 
                             </div></div>
 
                             <div> <div className="col-span-2 ">
-                                <div className="row-span-3 h-[330px]">
-                                    <div className=" h-full bg-[url('/public/15.jpg')] lg:bg-cover bg-center p-7">
-                                        <h1 className=' text-white text-2xl font-bold'>Mens Jacket</h1>
-                                        <h1 className=' text-white text-lg font-bold'>Explore</h1>
+                                <div className="row-span-3 lg:h-[330px] h-[150px]">
+                                    <div className=" h-full bg-[url('/public/15.jpg')] lg:bg-cover bg-center  p-7">
+                                        <h1 className=' text-white lg:text-2xl text-sm font-bold'>Mens Jacket</h1>
+                                        <h1 className=' text-white lg:text-lg text-xs font-bold'>Explore</h1>
                                     </div>
                                 </div>
 
